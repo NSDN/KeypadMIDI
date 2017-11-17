aa80a23c2ef61fa8ad0e21cffcc116790b8b9946
drzzm32 <drzzm32@gmail.com>
2017-11-18 01:02:28 +0800

    fix

--- KeypadMIDI.ino
+++ KeypadMIDI.ino
@@ -9,10 +9,18 @@ void setup() {
 void loop() {
     if (Serial.available() > 0) {
         byte tag = Serial.read();
+        byte key = 0;
 
-        if (tag == 0xFF)
-            Keyboard.press(Serial.read());
-        else if (tag == 0x00)
-            Keyboard.release(Serial.read());
+        if (tag == 0xFF) {
+            key = Serial.read();
+            if (key >= 0x41 && key <= 0x5A)
+                key += 0x20;
+            Keyboard.press(key);
+        } else if (tag == 0x00) {
+            key = Serial.read();
+            if (key >= 0x41 && key <= 0x5A)
+                key += 0x20;
+            Keyboard.release(key);
+        }
     }
 }
